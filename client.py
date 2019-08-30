82cbdf1762363c5504d106c4da418f53c815c983
Michael Stypa <michael@stypa.info>
2019-08-30 16:20:23 +0200

    linting and more structure

--- client.py
+++ client.py
@@ -1,37 +1,63 @@
 #!/usr/bin/env python2
 
-import dumpconfig as cfg
-import requests
-import json
-import csv
-from StringIO import StringIO
+"""Fetches a opencast statistics dump in a robust way and saves result as csv file."""
 
-URL = 'http://%s:%s/api/statistics/data/export.csv' % (cfg.opencast['host'], cfg.opencast['port'])
-AUTH = (cfg.opencast['user'], cfg.opencast['password'])
-DATA = {
-        'limit': 0,
-        'offset': 0,
+import sys
+import csv
+import json
+from StringIO import StringIO
+import requests
+import dumpconfig as cfg
+
+URL = 'http://%s:%s/api/statistics/data/export.csv' % (cfg.OPENCAST['host'], cfg.OPENCAST['port'])
+AUTH = (cfg.OPENCAST['user'], cfg.OPENCAST['password'])
+
+def get_csv_reader(offset):
+    """Return a csv reader object which will iterate over lines in the fetched
+    opencast csv statistics dump."""
+    data = {
+        'offset': offset,
         'filter': '',
+        'limit': cfg.APP['limit'],
         'data': json.dumps({
             'parameters': {
-                'resourceId': cfg.opencast['organization'],
-                'detailLevel': 'EPISODE',
-                'from': cfg.query['from'],
-                'to': cfg.query['to'],
-                'dataResolution': 'MONTHLY'
+                'resourceId': cfg.QUERY['resourceId'],
+                'detailLevel': cfg.QUERY['detailLevel'],
+                'from': cfg.QUERY['from'],
+                'to': cfg.QUERY['to'],
+                'dataResolution': cfg.QUERY['dataResolution'],
                 },
             'provider': {
-                'identifier': 'organization.views.sum.influx',
-                'resourceType': 'organization',
+                'identifier': cfg.QUERY['identifier'],
+                'resourceType': cfg.QUERY['resourceType'],
                 },
             }),
         }
+    response = requests.post(URL, auth=AUTH, data=data)
+    if int(response.status_code) != 200:
+        print 'HTTP response was %s.' % (response.status_code)
+        print response.text
+        if int(response.status_code) == 401:
+            print 'This can mean your credentials are incorrect.'
+            print 'This can mean the resourceID configured could not be found.'
+        sys.exit(1)
+    csvstring = json.loads(response.text)['csv']
+    return csv.reader(StringIO(csvstring), delimiter=',')
 
-r = requests.post(URL, auth=AUTH, data=DATA)
-#r.status_code
-csvstring = json.loads(r.text)['csv']
+def main():
+    """Reads pages from endpoint until no more data is received."""
+    reading = True
+    offset = 0
+    while reading:
+        reader = get_csv_reader(offset)
+        for row in reader:
+            '\t'.join(row)
+        print '%d lines read while parsing.' % (reader.line_num)
+        if reader.line_num == 0:
+            print 'End of dump.'
+            reading = False
+        else:
+            offset = offset + 1
 
-f = StringIO(csvstring)
-reader = csv.reader(f, delimiter=',')
-for row in reader:
-    print('\t'.join(row))
+if __name__ == "__main__":
+    main()
--- dumpconfig.py
+++ dumpconfig.py
@@ -1,11 +1,22 @@
-opencast = {
-        'host': 'localhost',
-        'port': '8080',
-        'user': 'admin',
-        'password': 'opencast',
-        'organization': 'mh_default_org',
-        }
-query = {
-        'from': '2018-01-01T00:00:00.000Z',
-        'to': '2020-01-01T00:00:00.000Z',
-        }
+"""1. Jaehrliche Statistik einer Serie mit Zahlen pro Episode (Brueckenkurs)"""
+
+APP = {
+    'limit': '2',
+    'fileprefix': 'fall1',
+    }
+OPENCAST = {
+    'host': 'localhost',
+    'port': '8080',
+    'user': 'admin',
+    'password': 'opencast',
+    'organization': 'mh_default_org',
+    }
+QUERY = {
+    'resourceId': '1c760d9a-26ce-48ad-b93e-cdda15ac5a5d',
+    'detailLevel': 'EPISODE',
+    'from': '2010-01-01T00:00:00.000Z',
+    'to': '2020-01-01T00:00:00.000Z',
+    'dataResolution': 'YEARLY',
+    'identifier': 'series.views.sum.influx',
+    'resourceType': 'series',
+    }
